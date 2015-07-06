432959650f012435a74cb6e4e6e0724b099bdc40
steveMQ <stevequatrale@gmail.com>
2015-07-06 16:48:05 -0400

    i modified the functions.php file

--- functions.php
+++ functions.php
@@ -1,1 +1,20 @@
 <?php
+
+// Our custom post type function
+function create_posttype() {
+
+	register_post_type( 'portfolio',
+	// CPT Options
+		array(
+			'labels' => array(
+				'name' => __( 'Portfolio' ),
+				'singular_name' => __( 'Portfolio' )
+			),
+			'public' => true,
+			'has_archive' => true,
+			'rewrite' => array('slug' => 'portfolio'),
+		)
+	);
+}
+// Hooking up our function to theme setup
+add_action( 'init', 'create_posttype' );
