d7af076e1627433634ec5137ddb34c9aa0999d9f
steveMQ <stevequatrale@gmail.com>
2015-07-07 17:20:45 -0400

    :fire: comments

--- partials/portfolio.php
+++ partials/portfolio.php
@@ -24,31 +24,4 @@
 
 
 
-  <!--a class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 port-hold" href="http://yankeelobstercompany.com">
-    <img class="project-pic" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/lobstahh.png" />
-    <div class="overlay">Yankee Lobster Company</div>
-  </a>
-
-  <a class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 port-hold" href="http://dev3.stevequatrale.com">
-    <img class="project-pic" id="creative" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/creative.png" />
-    <div class="overlay">Creative</div>
-  </a>
-
-  <a class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 port-hold" href="http://dev1.stevequatrale.com">
-    <img class="project-pic" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/swiss.png" />
-    <div class="overlay">Swiss</div>
-  </a>
-
-  <a class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 port-hold" href="http://dev2.stevequatrale.com">
-    <img class="project-pic" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/centy.png" />
-    <div class="overlay">Centy</div>
-  </a>
-
-  <a class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 port-hold" href="http://dev.stevequatrale.com">
-    <img class="project-pic" id="travl" src="<?php echo get_template_directory_uri(); ?>/assets/imgs/travlerLogo.png" />
-    <div class="overlay">Travler</div>
-  </a-->
-
-
-
 </section>
