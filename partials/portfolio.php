86bd0a344552963ea56a9952860f36dcdbf2c34b
steveMQ <stevequatrale@gmail.com>
2015-07-07 17:39:50 -0400

    finished up portfolio stuff

--- partials/portfolio.php
+++ partials/portfolio.php
@@ -2,26 +2,21 @@
 
   <h2 class="col-lg-12">previous work</h2>
 
+
   <?php
-
-    $args = array(
-        'post_type'=> 'portfolio'
-        );
-
-    $the_query = new WP_Query( $args );
-    if($the_query->have_posts() ) :
-      while ( $the_query->have_posts() ) :
-        $the_query->the_post();
-        $portfolioImage = get_field('portfolio_image');
+    global $post;
+    $portfolioItems = get_field('homepage_portfolio_item');
+    foreach ($portfolioItems as $post) :
+      $portfolioImage = get_field('portfolio_image');
   ?>
+
   <a class="col-xs-offset-1 col-xs-10 col-sm-4 col-md-3 port-hold" href="http://dev3.stevequatrale.com">
     <img class="project-pic" id="creative" src="<?php echo $portfolioImage['url']; ?>" />
     <div class="overlay"><?php the_title(); ?></div>
   </a>
-  <?php endwhile; ?>
-  <?php endif; ?>
-
-
-
+  <?php
+    endforeach;
+    wp_reset_query();
+  ?>
 
 </section>
